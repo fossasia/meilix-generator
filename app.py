1af16878e99f6762001a154d7e154563dc8000c3
Xeon Zolt <xeon.harsh@gmail.com>
2017-06-06 03:19:03 +0530

    response not found error

--- app.py
+++ app.py
@@ -13,9 +13,23 @@ def index():
 	#Index page
 	return render_template("index.html")
 
-#Function to call meilix script on clicking the build button	
+
+@app.route('/yield')
 def meilixCall():
-    subprocess.call(['./build.sh'])
+	def inner():
+		proc = subprocess.Popen(
+			['cmd'],             #call something with a lot of output so we can see it
+			shell=True,universal_newlines=True,
+			stdout=subprocess.PIPE
+		)
+
+		for line in iter(proc.stdout.readline,''):
+			time.sleep(1)                           # Don't need this just shows the text streaming
+			yield line.rstrip() + '<br/>\n'
+
+	return Response(inner(), mimetype='text/html')  # text/html is required for most browsers to show th$
+
+#Function to call meilix script on clicking the build button
 
 @app.route('/about')
 def about():
@@ -25,13 +39,13 @@ def about():
 #Return a custom 404 error.
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+	return 'Sorry, unexpected error: {}'.format(e), 404
 
 @app.errorhandler(500)
 def application_error(e):
-    #Return a custom 500 error.
-    return 'Sorry, unexpected error: {}'.format(e), 500
+	#Return a custom 500 error.
+	return 'Sorry, unexpected error: {}'.format(e), 500
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()

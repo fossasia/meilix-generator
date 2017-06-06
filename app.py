7f9712cfbfa338488f06cce9d6bf06b1784b4cdd
Harsh Lathwal <xeon.harsh@gmail.com>
2017-06-06 10:15:19 +0530

    button is working now (#10)
    Button to redirect to localhost/yield and gives the output

--- app.py
+++ app.py
@@ -1,27 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import time
+import subprocess
+
+# cloning meilix code
+#os.system("git clone https://github.com/fossasia/meilix.git")
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	"""Index page"""
+	#Index page
 	return render_template("index.html")
+
+
+@app.route('/yield')
+def output():
+	def inner():
+		proc = subprocess.Popen(
+			['dmesg'],             #call something with a lot of output so we can see it
+			shell=True,universal_newlines=True,
+			stdout=subprocess.PIPE
+		)
+
+		for line in iter(proc.stdout.readline,''):
+			time.sleep(1)                           # Don't need this just shows the text streaming
+			yield line.rstrip() + '<br/>\n'
+
+	return Response(inner(), mimetype='text/html')  # text/html is required for most browsers to show th$
+
+#Function to call meilix script on clicking the build button
 
 @app.route('/about')
 def about():
-	"""About page"""
+	#About page
 	return render_template("about.html")
 
+#Return a custom 404 error.
 @app.errorhandler(404)
 def page_not_found(e):
-    """Return a custom 404 error."""
-    return render_template("404.html"), 404
+	return 'Sorry, unexpected error: {}'.format(e), 404
 
 @app.errorhandler(500)
 def application_error(e):
-    """Return a custom 500 error."""
-    return 'Sorry, unexpected error: {}'.format(e), 500
+	#Return a custom 500 error.
+	return 'Sorry, unexpected error: {}'.format(e), 500
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()

26aea5987d6ef184b43d6ddbec113fdc49084c8a
Xeon Zolt <xeon.harsh@gmail.com>
2017-06-08 01:46:43 +0530

    Update script.sh Update script.sh
    ./build not found shit its build.sh
    Update script.sh
    
    adding sudo
    Update script.sh
    Update script.sh
    Create docker-compose.yml
    Merge pull request #2 from fossasia/master
    
    update

--- script.sh
+++ script.sh
@@ -1,7 +1,4 @@
-echo hello world 
-#to test
-
-git submodule init
-git submodule update
+echo hello world #to test
+git clone https://github.com/Meilix-Generator/meilix.git #submodule not working checked heroku logs 
 cd meilix
-./build
+./build.sh

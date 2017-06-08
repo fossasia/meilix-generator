845b5d023f06470784fa39f44c91bd1719951ea4
meets2tarun <meets2tarun@gmail.com>
2017-06-08 21:38:27 +0530

    token test

--- script.sh
+++ script.sh
@@ -1,3 +1,20 @@
-
-cd meili
-fakeroot -u ./build.sh
+#!/bin/bash
+cat travis_tokens | while read line;
+do
+    array=(${line})
+    user="${array[0]}"
+    project="${array[1]}"
+    len=${#array[@]}
+    for ((i=3; i<len; i++)); do
+        branch="${array[i]}"
+        body="{\"request\": {\"branch\":\"${branch}\"}}"
+        echo "Triggering build of ${user}'s ${project}/${branch} with token ${token}"
+        curl -s -X POST \
+            -H "Content-Type: application/json" \
+            -H "Accept: application/json" \
+            -H "Travis-API-Version: 3" \
+            -H "Authorization: token ${token}" \
+            -d "${body}" \
+            "https://api.travis-ci.org/repo/${user}%2F${project}/requests"
+    done
+done

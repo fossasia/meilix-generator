1b5b93377ef624f2b21f9befb3af5fe8c70c572b
Tarun Kumar <meets2tarun@gmail.com>
2017-06-11 23:18:48 +0530

    Create script.sh

--- script.sh
+++ script.sh
@@ -16,6 +16,7 @@ do
                 }
             }
     }}"
+    echo "https://github.com/xeon-zolt/meilix/releases/download/${TRAVIS_TAG}/meilix-zesty-`date +%Y%m%d`-i386.iso"
         curl -s -X POST \
             -H "Content-Type: application/json" \
             -H "Accept: application/json" \

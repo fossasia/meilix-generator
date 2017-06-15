d2e7e0230ca85971cafd836741419a7edb1bf6d8
Xeon Zolt <xeon.harsh@gmail.com>
2017-06-15 09:17:44 +0530

    minor improvement in urlify function

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def allowed_file(filename):
 
 def urlify(s):
      # Remove all non-word characters (everything except numbers and letters)
-     s = re.sub(r"[^\w\s]", '', s)
+     s = re.sub(r"[^\w\s]", '', s).strip()
      # Replace all runs of whitespace with a single dash
      s = re.sub(r"\s+", '-', s)
      return s

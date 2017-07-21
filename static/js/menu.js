724c6cbaed0d0c2f122d4d45a2bac55cf395c3b1
Xeon Zolt <xeon.harsh@gmail.com>
2017-07-21 10:16:14 +0530

    added menu and UI changes

---
+++ static/js/menu.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+		$(document).delegate('.open', 'click', function(event){
+			$(this).addClass('oppenned');
+			event.stopPropagation();
+		})
+		$(document).delegate('body', 'click', function(event) {
+			$('.open').removeClass('oppenned');
+		})
+		$(document).delegate('.cls', 'click', function(event){
+			$('.open').removeClass('oppenned');
+			event.stopPropagation();
+		});
+	});

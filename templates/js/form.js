02517879e18e96f6f0442ad3ad52c69769502c71
meets2tarun <meets2tarun@gmail.com>
2018-05-22 10:58:50 +0530

    added double quotes

--- templates/js/form.js
+++ templates/js/form.js
@@ -1,17 +1,17 @@
 /* global $ */
 
-  var customMenuButton = $('.custom-menubutton');
-  var menuContent = $('.custom-menu-cont');
-  var fontAwesomeIcom=$('.glyphicon-th');
+  var customMenuButton = $(".custom-menubutton");
+  var menuContent = $(".custom-menu-cont");
+  var fontAwesomeIcom=$(".glyphicon-th");
 
   customMenuButton.click(function() {
     menuContent.toggleClass("hidden");
-    $(this).toggleClass('custom-menubutton-color');
+    $(this).toggleClass("custom-menubutton-color");
   });
 
   $('.custom-menu-item').click(function () {
-     menuContent.addClass('hidden');
-     customMenuButton.removeClass('custom-menubutton-color');
+     menuContent.addClass("hidden");
+     customMenuButton.removeClass("custom-menubutton-color");
   });
 
   $(document).mouseup(function(e) {
@@ -19,6 +19,6 @@
     // the container, or descendants of the container
     if (!$(e.target).is(customMenuButton) && !$(e.target).is(menuContent) && menuContent.has(e.target).length === 0 && !$(e.target).is(fontAwesomeIcom)) {
       menuContent.addClass("hidden");
-      customMenuButton.removeClass('custom-menubutton-color');
+      customMenuButton.removeClass("custom-menubutton-color");
     }
   });

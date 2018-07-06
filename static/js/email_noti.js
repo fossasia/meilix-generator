2e6f13e0e64a2d4bed06fb53a3f98cf456e61272
ROHIT POTTER <rohitpotter12@gmail.com>
2018-07-07 00:03:50 +0530

    solving dropdown issue

--- static/js/email_noti.js
+++ static/js/email_noti.js
@@ -1,7 +1,11 @@
-function hideDiv(){
-    document.getElementsByClassName("custom-menu-cont")[0].classList.toggle("hidden");
-}
-document.addEventListener("click", hideDiv);
+$(document).on('click', function() {
+  $('.custom-menu-cont').toggleClass('hidden');
+});
+
+$('.custom-menubutton').on('click', function(e) {
+  e.stopPropagation();
+  $('.custom-menu-cont').toggleClass('hidden');
+});
 function noti()
 {
 	alert("Email is used to mail user the link to the build ISO");

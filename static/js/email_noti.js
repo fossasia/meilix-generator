09ada4c16db409569781b10e77cdbb2fb64c93fe
Dhavalkumar Prajapati <dhaval.daiict@gmail.com>
2018-07-06 15:53:06 +0530

    index.html made it readable css and js added in separate file (#186)
    * index.html made it readable
    
    * Update email_noti.js
    
    * Update email_noti.js
    
    * Update email_noti.js

---
+++ static/js/email_noti.js
@@ -0,0 +1,8 @@
+function hideDiv(){
+    document.getElementsByClassName("custom-menu-cont")[0].classList.toggle("hidden");
+}
+document.addEventListener("click", hideDiv);
+function noti()
+{
+	alert("Email is used to mail user the link to the build ISO");
+}

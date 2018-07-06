7810699659bd7acf1e1c093ff2f4c4ceb5e8867a
ROHIT POTTER <rohitpotter12@gmail.com>
2018-07-07 00:42:51 +0530

    working on issue implementation of help icon

--- static/js/email_noti.js
+++ static/js/email_noti.js
@@ -1,5 +1,20 @@
-
-function noti()
+function email()
 {
 	alert("Email is used to mail user the link to the build ISO");
+}
+function disti()
+{
+	alert("This will be name of the distribution");
+}
+function hpage()
+{
+	alert("This will be the home page of the browser");
+}
+function sengine()
+{
+	alert("This will be the default search engine of the browser");
+}
+function pinstall()
+{
+	alert("packages will installed which you have checked");
 }

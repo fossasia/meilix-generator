17717ae5e2b9be52099f1d32c76960708bde0861
ROHIT POTTER <rohitpotter12@gmail.com>
2018-07-07 10:38:17 +0530

    dropdown issue

--- static/js/email_noti.js
+++ static/js/email_noti.js
@@ -1,7 +1,11 @@
-function noti()
-{
-	alert("Email is used to mail user the link to the build ISO");
-}
+$(document).on('click', function() {
+  $('.custom-menu-cont').toggleClass('hidden');
+});
+
+$('.custom-menubutton').on('click', function(e) {
+  e.stopPropagation();
+  $('.custom-menu-cont').toggleClass('hidden');
+});
 function disti()
 {
 	alert("This will be name of the distribution");

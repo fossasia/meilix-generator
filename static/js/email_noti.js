c4750426929c343a301b80c12cb36da1834a3b21
Dhavalkumar Prajapati <dhaval.daiict@gmail.com>
2018-07-08 12:40:25 +0530

    Update email_noti.js

--- static/js/email_noti.js
+++ static/js/email_noti.js
@@ -1,6 +1,3 @@
-$(document).on("click", ".glyphicon-th", function () {
-    document.getElementsByClassName("custom-menu-cont")[0].classList.toggle("hidden");
-});
 function noti()
 {
 	alert("Email is used to mail user the link to the build ISO");

cc8ec618a9d0289f079ebbaa53a2f2ec9c4cb176
adhavalboy <dhaval.daiict@gmail.com>
2018-07-08 12:31:12 +0530

    loader added and some css modification made

---
+++ static/js/alert.js
@@ -0,0 +1,4 @@
+function noti()
+{
+	alert("Email is used to mail user the link to the build ISO");
+}
---
+++ static/js/functions.js
@@ -0,0 +1,14 @@
+/*add all function script here*/
+
+/*<----start loader script---->*/
+$("nav").hide();
+				$(document).ready(function(){
+					$("nav").show();
+			 $(".welcome").fadeOut("slow", function() { $(this).css({visibility: "hidden", opacity: 0})});
+       });
+/*<----loader script End---->*/
+
+/*<----Drop down menu show hide---->*/
+$(document).on("click", ".glyphicon-th", function () {
+    document.getElementsByClassName("custom-menu-cont")[0].classList.toggle("hidden");
+});

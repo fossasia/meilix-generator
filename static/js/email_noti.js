dec8969b4b08bedde90b9fd2602e7b0b972e53a8
Dhavalkumar Prajapati <dhaval.daiict@gmail.com>
2018-07-08 08:35:40 +0530

    table modified  and drop down menu issue solved    (#189)
    * index.html made it readable
    
    * Update email_noti.js
    
    * Update email_noti.js
    
    * Update email_noti.js
    
    * table modified
    
    * Update style.css
    
    * drop down menu toggle solved
    
    * Update email_noti.js

--- static/js/email_noti.js
+++ static/js/email_noti.js
@@ -1,7 +1,6 @@
-function hideDiv(){
+$(document).on("click", ".glyphicon-th", function () {
     document.getElementsByClassName("custom-menu-cont")[0].classList.toggle("hidden");
-}
-document.addEventListener("click", hideDiv);
+});
 function noti()
 {
 	alert("Email is used to mail user the link to the build ISO");

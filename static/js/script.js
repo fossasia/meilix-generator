d617578b8dd9e7cab53575c3b07c8e4785133ce2
Nitesh Kumar Niranjan <niteshkumarniranjan@gmail.com>
2018-12-15 17:17:21 +0530

    Added Dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,10 @@
-var customFileInputBox = document.querySelectorAll('.custom__file__input'); 
+var customFileInputBox = document.querySelectorAll(".custom__file__input"); 
+var dropdownButton = document.querySelector(".drop-btn");
+var dropdown = document.querySelector(".drop-btn .dropdown");
 
 function addFileEventListeners(realInput, customTextBox, customButton) {
 
-  customButton.addEventListener('click', function(e) {
+  customButton.addEventListener("click", function(e) {
     e.preventDefault();
     realInput.click();
     return false;
@@ -20,4 +22,12 @@ function addFileEventListeners(realInput, customTextBox, customButton) {
 
 customFileInputBox.forEach(function(el) {
   addFileEventListeners(el.children[0], el.children[1], el.children[2]);
+});
+
+document.addEventListener("click", function(e) {
+  if (e.target.id === "img_btn") {
+    dropdown.classList.toggle("visible");
+  } else {
+    dropdown.classList.remove("visible");
+  }
 });

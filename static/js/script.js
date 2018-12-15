783dc57499546ab3a8f7350ed37e07e5d1a22cc1
Nitesh Kumar Niranjan <niteshkumarniranjan@gmail.com>
2018-12-15 14:41:31 +0530

    Change ES6 arrow function to ES5 functions

--- static/js/script.js
+++ static/js/script.js
@@ -2,13 +2,13 @@ var customFileInputBox = document.querySelectorAll('.custom__file__input');
 
 function addFileEventListeners(realInput, customTextBox, customButton) {
 
-  customButton.addEventListener('click', e => {
+  customButton.addEventListener('click', function(e) {
     e.preventDefault();
     realInput.click();
     return false;
   });
 
-  realInput.addEventListener("change", e => {
+  realInput.addEventListener("change", function(e) {
     if (realInput.value) {
       customTextBox.innerHTML = realInput.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
     } else {
@@ -18,6 +18,6 @@ function addFileEventListeners(realInput, customTextBox, customButton) {
 
 }
 
-customFileInputBox.forEach(el => {
+customFileInputBox.forEach(function(el) {
   addFileEventListeners(el.children[0], el.children[1], el.children[2]);
 });
